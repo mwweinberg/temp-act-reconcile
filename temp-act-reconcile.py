c534cebfea26271e0c44cf367b9fe7a1424e1310
Michael Weinberg <hello@michaelweinberg.org>
2017-07-16 11:36:19 -0400

    initial commit with flow plan

---
+++ temp-act-reconcile.py
@@ -0,0 +1,14 @@
+
+
+# TODO: import CSV into lists
+# old list
+# new list
+
+
+# TODO: Compare new list to old list
+# if element is not in old list, add to output list
+
+
+#TODO: Format output with spaces
+
+#TODO: output to CSV
