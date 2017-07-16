e5b4048a3295a3fe667b5f0e414b7070c6be3e63
Michael Weinberg <hello@michaelweinberg.org>
2017-07-16 12:41:53 -0400

    added comparison

--- temp-act-reconcile.py
+++ temp-act-reconcile.py
@@ -1,11 +1,12 @@
 import csv
 
+#creates the output list
+output_csv_list = []
+
 #open the output doc as writable
 
 ouput_csv = open("output.csv", "w")
 
-
-# TODO: import CSV into lists
 #turns csvs into lists of lists
 
 with open('old.csv', 'rU') as f:
@@ -14,18 +15,28 @@ with open('old.csv', 'rU') as f:
     #removes the ordinals for each entry because they are not relevant
     for element in old_csv_list:
         del element[0]
-    print(old_csv_list)
+    #print(old_csv_list)
 
 with open('new.csv', 'rU') as f:
     reader = csv.reader(f)
     new_csv_list = list(list(rec) for rec in csv.reader(f, delimiter=','))
     for element in new_csv_list:
         del element[0]
-    print(new_csv_list)
+    #print(new_csv_list)
 
 
 # TODO: Compare new list to old list
 # if element is not in old list, add to output list
+
+for element in new_csv_list:
+    if element in old_csv_list:
+        print('existing entry')
+    else:
+        print('new entry')
+        output_csv_list.append(element)
+
+print(output_csv_list)
+
 
 
 #TODO: Format output with spaces
